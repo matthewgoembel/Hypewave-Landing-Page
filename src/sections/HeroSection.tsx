8bbd6e52b77f7b9a4d321eb2a47ca830946fe035
MattyGPu <mgoembel1@gmail.com>
2025-08-29 15:36:52 -0400

    coming soon

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -43,14 +43,11 @@ export default function HeroSection() {
       <div className="-mt-0 flex flex-col items-center gap-3">
         <button className="flex items-center gap-3 bg-white text-black px-5 py-2 rounded-full shadow hover:scale-105 transition">
           <Image src="/icons/appstore.svg" alt="Chrome" width={48} height={48} />
-          Download on the Appstore
+          Coming Soon on the Appstore
         </button>
         <p className="text-center text-sm md:text-base text-[#4f4f5a] font-normal leading-snug">
           Also available on other browsers and devices.
           <br />
-          <a href="#" className="underline hover:text-[#3ABEFF] transition">
-            Discover more
-          </a>
         </p>
       </div>
     </section>
--- src/sections/OutroSection.tsx
+++ src/sections/OutroSection.tsx
@@ -30,7 +30,7 @@ export default function HeroSection() {
       <div className="mt-10 flex flex-col items-center gap-3">
         <button className="flex items-center gap-3 bg-white text-black px-5 py-2 rounded-full shadow hover:scale-105 transition">
           <Image src="/icons/appstore.svg" alt="Chrome" width={48} height={48} />
-          Download on the Appstore
+          Coming Soon on the Appstore
         </button>
         <p className="text-center text-sm md:text-base text-[#4f4f5a] font-normal leading-snug">
         Also available on other browsers and devices.
